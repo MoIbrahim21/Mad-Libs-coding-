186f0b56f9535415bff6cc85701375621688ae7a
Mo <42975535+MoIbrahim21@users.noreply.github.com>
2018-10-08 12:44:52 -0400

    Add files via upload

---
+++ src/Madlibsproject.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Madlibsproject {
+
+	public static void main(String[] args) 
+	{
+		Scanner verb = new Scanner(System.in);
+		System.out.print("Please enter a noun:   ");		
+		String noun = verb.nextLine();
+		System.out.print("\n Please enter a verb:  ");
+		String v1 = verb.nextLine();
+		System.out.print("\n Please enter an adjective:   ");
+		String adj1 = verb.nextLine();
+		System.out.print("\n Please enter a location:  ");
+		String location = verb.nextLine();
+		System.out.print("\n Use (At) first before listing location");
+		String specs = verb.nextLine();
+		System.out.print("\n Please enter a time:  ");
+		String time = verb.nextLine();
+		System.out.println("Please enter a noun:  ");
+		String noun2 = verb.nextLine();
+		System.out.print("Please enter a disgusting verb:  ");
+
+	}
+
+}
