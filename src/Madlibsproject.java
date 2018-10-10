120fb5fe8aee224853a3bc0fda53dc247be5fb1d
Mo <42975535+MoIbrahim21@users.noreply.github.com>
2018-10-09 21:56:10 -0500

    Add files via upload

--- src/Madlibsproject.java
+++ src/Madlibsproject.java
@@ -13,14 +13,24 @@ public class Madlibsproject {
 		String adj1 = verb.nextLine();
 		System.out.print("\n Please enter a location:  ");
 		String location = verb.nextLine();
-		System.out.print("\n Use (At) first before listing location");
-		String specs = verb.nextLine();
 		System.out.print("\n Please enter a time:  ");
 		String time = verb.nextLine();
-		System.out.println("Please enter a noun:  ");
+		System.out.println("\n please enter a noun:  ");
 		String noun2 = verb.nextLine();
-		System.out.print("Please enter a disgusting verb:  ");
-
+		System.out.print("\n Please enter a disgusting verb:  ");
+		String v2 = verb.nextLine();
+		System.out.print("\n Please enter a noun:  ");
+		String noun3 = verb.nextLine();
+		System.out.print("\n Please enter an adjective:  ");
+		String adj2 = verb.nextLine();
+		System.out.print("\n Please enter a weird verb:  ");
+		String v3 = verb.nextLine();
+		System.out.print("\n Please enter a noun:  ");
+		String noun4 = verb.nextLine();
+		System.out.print("\n Please enter a place:  ");
+		String location2 = verb.nextLine();
+		System.out.println("There once was a Noun who was funny and liked to V1 in a Adj1 way. This ussually took place at Location during a time with noun2 who also v2.");
+		
 	}
 
 }
